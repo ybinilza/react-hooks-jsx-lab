49a090b344e371c6184f03ec8777da05f974c44b
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-05-21 21:11:59 -0400

    lab completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const divStyle = {
+  color: 'firebrick',
+};
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (<div id="home">
+            <h1 style={divStyle}>{name} is a Web Developer from {city}</h1>
+
+  </div>);
 }
 
 export default Home;
